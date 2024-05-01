f8cbda917837543b6186c4a355684d220d7a3e25
michelle-curtis <mcurtis459@gmail.com>
2024-05-01 15:28:30 -0400

    moved starcat to src

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="starCAT",
+    version="1.0",
+    author="Dylan Kotliar, Michelle Curtis",
+    author_email="dylkot@gmail.com, curtism@broadinstitute.org",
+    description="Implements *CellAnnotator (aka *CAT/starCAT), annotating scRNA-Seq with predefined gene expression programs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/immunogenomics/starCAT",
+    project_urls={
+        "Bug Tracker": "https://github.com/immunogenomics/starCAT/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    entry_points={
+        'console_scripts': [
+            'starCAT = starCAT:main',
+        ],
+    },
+    install_requires=[
+   'scikit-learn>=1.0',
+   'scanpy',
+   'pandas',
+   'numpy',
+   'fastcluster',
+   'matplotlib',
+   'palettable',
+   'scipy',
+   'pyyaml',
+   'cnmf'
+   ]
+)
---
+++ src/starcat/__init__.py
@@ -0,0 +1,1 @@
+from .starcat import main, starCAT

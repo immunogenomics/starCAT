7d34b89116e25ea40c88616de08ed4f035a8379a
Dylan Kotliar <dylkot@gmail.com>
2024-10-25 11:07:28 -0400

    expose starcat.availabe_refs

--- src/starcat/__init__.py
+++ src/starcat/__init__.py
@@ -1,3 +1,3 @@
-from .starcat import main, starCAT, load_df_from_npz, reference_url
+from .starcat import main, starCAT, load_df_from_npz, reference_url, available_refs, ref_list
 from .build_consensus_reference import BuildConsensusReference
 from .utils import decompress_tar
